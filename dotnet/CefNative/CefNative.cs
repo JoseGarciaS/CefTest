bbcb5ae22bf439f50af06b549c9d22bf261e9da2
JoseGarciaS <72817489+JoseGarciaS@users.noreply.github.com>
2026-05-27 18:33:06 -0600

    Path fixing

--- dotnet/CefNative/CefNative.cs
+++ dotnet/CefNative/CefNative.cs
@@ -27,42 +27,32 @@ public static class Cef
         string browserSubprocessPath);
 
     private static IntPtr ResolveLibrary(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
-    {
-        if (libraryName != LibName)
-            return IntPtr.Zero;
+{
+    if (libraryName != LibName)
+        return IntPtr.Zero;
 
-        var nativeDir = GetRuntimeNativeFolder();
-        var libFileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-            ? "CefNative.dll"
-            : RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
-                ? "libCefNative.dylib"
-                : "libCefNative.so";
+    var libFileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+        ? "CefNative.dll"
+        : RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+            ? "libCefNative.dylib"
+            : "libCefNative.so";
 
-        return NativeLibrary.Load(Path.Combine(nativeDir, libFileName));
-    }
-
-    private static string GetRuntimeNativeFolder()
-    {
-        var rid = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-            ? "win-x64"
-            : RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
-                ? "osx-x64"
-                : "linux-x64";
-        return Path.Combine(AppContext.BaseDirectory, "runtimes", rid, "native");
-    }
+    var fullPath = Path.Combine(AppContext.BaseDirectory, libFileName);
+    return File.Exists(fullPath) ? NativeLibrary.Load(fullPath) : IntPtr.Zero;
+}
 
     public static int Run(CefOptions? options = null)
-    {
-        options ??= new CefOptions();
+{
+    options ??= new CefOptions();
 
-        var nativeDir = GetRuntimeNativeFolder();
-        var cacheDir = options.CacheDir ?? Path.Combine(AppContext.BaseDirectory, "cef_cache");
-        var subprocessPath = Path.Combine(nativeDir, RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-            ? "cef_subprocess.exe"
-            : "cef_subprocess");
+    var baseDir = AppContext.BaseDirectory;
+    var cacheDir = options.CacheDir ?? Path.Combine(baseDir, "cef_cache");
+    var subprocessPath = Path.Combine(baseDir, RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+        ? "cef_subprocess.exe"
+        : "cef_subprocess");
 
-        string[] argv = [AppContext.BaseDirectory];
+    string[] argv = [baseDir];
 
-        return _RunWithSubprocess(argv.Length, argv, nativeDir, nativeDir, cacheDir, subprocessPath);
-    }
+    return _RunWithSubprocess(argv.Length, argv, baseDir, baseDir, cacheDir, subprocessPath);
+}
 }
